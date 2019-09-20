7375006ab09f160bbbcad1eb209dc4f255e50fa1
joostlek <joostlek@outlook.com>
2019-09-20 22:30:09 +0200

    Add partner list

---
+++ pages/partners/_partner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ImageBlock
+      :title="partner.name"
+      :image="partner.logoUrl"
+      :type="'image-right'"
+      :text="''"
+    />
+    <TextBlock
+      :type="'text-left'"
+      :text="partner.description"
+      :contained="true"
+      :button="{
+        'url': partner.url,
+        'text': 'Ga naar de website'
+      }"
+    />
+  </div>
+</template>
+
+<script>
+import content from '../../content.json'
+import TextBlock from '../../components/TextBlock'
+import ImageBlock from '../../components/ImageBlock'
+
+export default {
+  name: 'Partner',
+  components: { ImageBlock, TextBlock },
+  data: () => ({
+    partner: {}
+  }),
+  validate({ params }) {
+    return content.partners.some(partner => partner.slug === params.partner)
+  },
+  mounted() {
+    const pageData = content.partners.find(p => p.slug === this.$route.params.partner)
+    this.$set(this, 'partner', pageData)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/partners/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="partner-list container">
+    <PartnerTile v-for="(partner, idx) in partners" :key="idx" :partner="partner" />
+  </div>
+</template>
+
+<script>
+import content from '../../content.json'
+import PartnerTile from '../../components/PartnerTile'
+
+export default {
+  name: 'Index',
+  components: { PartnerTile },
+  data: () => ({
+    partners: content.partners
+  })
+}
+</script>
+
+<style scoped lang="scss">
+  .partner-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
